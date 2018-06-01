e9d84ddaba22333a5f05866d8ec6282b1724b39b
cat-125 <thepennyishiny@yahoo.com>
2018-06-01 12:03:50 -0700

    update

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
-    $(document).on("scroll", onScroll);
+  $(document).on("scroll", onScroll);
 });
 
 function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('#menu a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#menu ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
+  var scrollPos = $(document).scrollTop();
+  $('#menu a').each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      $('#menu ul li a').removeClass("active");
+      currLink.addClass("active");
+    }
+    else{
+    currLink.removeClass("active");
+    }
+  });
 }
+
+$('.hidden').removeClass('hidden').hide();
+$('.toggle-text').click(function() {
+  $(this).find('span').each(function() { $(this).toggle(); });
+});
